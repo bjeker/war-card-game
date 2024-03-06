8e240551f4998bedb55b1451dcb013aee02e5117
bjeker <rbieke1@gmail.com>
2024-03-05 19:58:14 -0600

    added classes and skeleton for Leaderboard class

---
+++ Leaderboard.java
@@ -0,0 +1,34 @@
+public class Leaderboard
+{
+    int topScores;
+
+    void leaderboard()
+    {
+
+    }
+
+    void display()
+    {
+
+    }
+
+    int getMaxScore()
+    {
+        return 0;
+    }
+
+    int getMinScore()
+    {
+        return 0;
+    }
+
+    void addScore()
+    {
+
+    }
+
+    void removeScore()
+    {
+
+    }
+}
